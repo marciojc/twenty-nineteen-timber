ae5f9e09e43b1dff5aa02432c4ee55a006371882
marciojc <marciojc@gmail.com>
2019-03-01 18:44:55 +0000

    Update site-featured-image

--- footer.php
+++ footer.php
@@ -12,9 +12,11 @@
  */
 
 $timberContext = $GLOBALS['timberContext']; // @codingStandardsIgnoreFile
+
 if ( ! isset( $timberContext ) ) {
-  throw new \Exception( 'Timber context not set in footer.' );
+ 	throw new \Exception( 'Timber context not set in footer.' );
 }
+
 $timberContext['content'] = ob_get_contents();
 ob_end_clean();
 $templates = array( 'page-plugin.twig' );
